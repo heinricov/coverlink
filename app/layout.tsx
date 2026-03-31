2a375f59f4037ecfce72ecf3204c483c2c06ff99
heinricov abraham <heinricoov@gmail.com>
2026-03-31 18:01:00 +0700

    update title

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,7 +17,7 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "COVERLINK",
+  title: "COVER LINK",
   description:
     "Buat kombinasi tautan otomatis lalu buka atau kelola semuanya sekaligus.",
 };
